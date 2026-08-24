468304c0e529cc647d025f1e41a6e10627ebd0ad
Bran <branz7377@gmail.com>
2026-08-23 23:55:41 -0600

    Proyecto calculo de sueldo

---
+++ SueldoSemanal-Bran/SueldoSemanal-Bran/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace SueldoSemanal_Bran;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void CalcularSueldo_Clicked(object sender, EventArgs e)
+    {
+        double horasTrabajadas;
+        double pagoPorHora;
+
+        bool horasCorrectas = double.TryParse(horas.Text, out horasTrabajadas);
+        bool pagoCorrecto = double.TryParse(pagoHora.Text, out pagoPorHora);
+
+        if (horasCorrectas && pagoCorrecto && horasTrabajadas >= 0 && pagoPorHora >= 0)
+        {
+            double sueldo = horasTrabajadas * pagoPorHora;
+
+            resultado.Text = "El sueldo semanal es: $" + sueldo.ToString("0.00");
+        }
+        else
+        {
+            resultado.Text = "Ingrese valores válidos.";
+        }
+    }
+}
---
+++ SueldoSemanal-Bran/SueldoSemanal-Bran/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SueldoSemanal_Bran
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
