44af8a99963e59276fdd25a74295700334442b91
Bran <branz7377@gmail.com>
2026-08-23 23:50:51 -0600

    Proyecto NumerosPares

---
+++ NumerosPares-Bran/NumerosPares-Bran/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NumerosPares_Bran
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NumerosPares-Bran/NumerosPares-Bran/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace NumerosPares_Bran;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void MostrarPares_Clicked(object sender, EventArgs e)
+    {
+        string numeros = "";
+
+        for (int i = 0; i <= 100; i = i + 2)
+        {
+            numeros = numeros + i + "\n";
+        }
+
+        resultado.Text = numeros;
+    }
+}
